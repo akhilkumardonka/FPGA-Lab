a67cf65fd6c5202abc5c3b17305477d6edc1544e
Akhil Kumar <akhilkumar1299@gmail.com>
2022-01-13 13:04:57 +0530

    added c coding for assgn 1

---
+++ Assignment 1/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    unsigned char G; //output
+    unsigned char one = 0x01;//used for displaying the output in bit
+    unsigned char  U=0x01,V=0x00,W=0x00; //inputs in hex
+
+    G = (U | V | (~W))&(U | (~V) | (~W))&((~U) | V | (~W))&((~U) | (~V) | W);
+
+    printf("%x%x%x",one&U,one&V,one&W);
+    printf(" ");
+    printf("%x\n" ,one&G);
+    return 0;
+}
