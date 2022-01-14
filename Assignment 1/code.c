939966cdc97232b4e4ae60178330fcde67482159
Akhil Kumar <akhilkumar1299@gmail.com>
2022-01-14 12:12:42 +0530

    minimized POS

--- Assignment 1/code.c
+++ Assignment 1/code.c
@@ -5,7 +5,7 @@ int main(){
     unsigned char one = 0x01;//used for displaying the output in bit
     unsigned char  U=0x01,V=0x00,W=0x00; //inputs in hex
 
-    G = (U | V | (~W))&(U | (~V) | (~W))&((~U) | V | (~W))&((~U) | (~V) | W);
+    G = (U | (~W))&(V | (~W))&((~U) | (~V) | W);
 
     printf("%x%x%x",one&U,one&V,one&W);
     printf(" ");
