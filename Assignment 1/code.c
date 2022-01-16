9c5fcd83a80ad24ecab93190689e7bad3013f78a
Akhil Kumar <akhilkumar1299@gmail.com>
2022-01-16 11:05:26 +0530

    revised C code

--- Assignment 1/code.c
+++ Assignment 1/code.c
@@ -5,7 +5,11 @@ int main(){
     unsigned char one = 0x01;//used for displaying the output in bit
     unsigned char  U=0x01,V=0x00,W=0x00; //inputs in hex
 
-    G = (U | (~W))&(V | (~W))&((~U) | (~V) | W);
+    // POS Expression 
+    // G = (U | (~W))&(V | (~W))&((~U) | (~V) | W);
+
+    // SOP Expression & used for NAND implementation
+    G = (~(U&V))&(~W) | (U&V&W);
 
     printf("%x%x%x",one&U,one&V,one&W);
     printf(" ");
